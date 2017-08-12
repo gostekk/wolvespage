8a5a85cefb5ada5d2a83a81d0f6185dd387d4cbd
gostekk <gostekk94@gmail.com>
2017-08-13 00:09:29 +0200

    Added dashboard things and updated some pages

--- public/js/admins.js
+++ public/js/admins.js
@@ -6,3 +6,19 @@ async function refreshTable(table) {
   await sleep(400);
   $(table).bootstrapTable('refresh');
 }
+
+admins = {
+  showNotification: function(notifyType, msg){
+    	$.notify({
+        	icon: "notifications",
+        	message: msg,
+        },{
+            type: notifyType,
+            timer: 3000,
+            placement: {
+                from: 'top',
+                align: 'center'
+            }
+        });
+	}
+}
